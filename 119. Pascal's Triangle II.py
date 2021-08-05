7dba361ff857dcaee7df95331c576572e67fe3ef
Tanjina Rahman <tanjina.3ni@gmail.com>
2021-08-05 16:13:34 +0600

    119

---
+++ 119. Pascal's Triangle II.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        L = [[1],[1,1]]
+        for i in range(0,rowIndex+1):
+            l = [1]*(i+1)
+
+            if i>1:
+                for j in range(1,len(l)-1):
+                    l[j] = L[i-1][j-1]+L[i-1][j]
+                L.append(l)
+        return l
